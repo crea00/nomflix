084a9a3189ba6ac249a8ac19bc523b23ab3ffe38
Jerome <654321su@gmail.com>
2019-03-16 20:45:05 +0900

    Create Poster Component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Section from '../../Components/Section';
 import Loader from '../../Components/Loader';
 import Message from '../../Components/Message';
+import Poster from '../../Components/Poster';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -17,7 +18,7 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster />
           ))}
         </Section>
       )}
@@ -25,7 +26,7 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movie">
           {upcoming.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster />
           ))}
         </Section>
       )}
@@ -33,7 +34,7 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
       {popular && popular.length > 0 && (
         <Section title="Popular Movie">
           {popular.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster />
           ))}
         </Section>
       )}
